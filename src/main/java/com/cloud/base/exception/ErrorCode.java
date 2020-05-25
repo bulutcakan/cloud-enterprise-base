9799724c5d27a1f1ab1f4f6c6bdcd43ec00155d7
bulutcakan <bcakan@migros.com.tr>
2020-05-26 01:43:04 +0300

    removed

--- src/main/java/com/cloud/base/exception/ErrorCode.java
+++ src/main/java/com/cloud/base/exception/ErrorCode.java
@@ -27,42 +27,6 @@ public final class ErrorCode extends AbstractErrorCode {
         ErrorCode SERVICE_UNAVAILABLE = new ErrorCode(99);
     }
 
-    @Section(min = 100, max = 1000)
-    public interface Session {
-        ErrorCode SESSION_NOT_FOUND = new ErrorCode(100);
-        ErrorCode INVALID_ACCESS = new ErrorCode(101);
-        ErrorCode USER_SESSION_NOT_FOUND = new ErrorCode(102);
-    }
-
-
-
-    @Section(min = 2000, max = 3000)
-    public interface Role {
-        ErrorCode INVALID_ROLE = new ErrorCode(2000);
-        ErrorCode ROLE_NOT_EXIST = new ErrorCode(2001, "Role not found!");
-        ErrorCode ROLE_NAME_ALREADY_EXIST = new ErrorCode(2002, "Role name already exist!");
-        ErrorCode ROLE_NOT_SAVED = new ErrorCode(2003);
-        ErrorCode ROLE_NOT_UPDATED = new ErrorCode(2004);
-        ErrorCode ROLE_NOT_DELETED = new ErrorCode(2005);
-    }
-
-    @Section(min = 3000, max = 4000)
-    public interface Application {
-        ErrorCode INVALID_APPLICATION = new ErrorCode(3000);
-        ErrorCode APPLICATION_NOT_EXIST = new ErrorCode(3001, "Application not found!");
-        ErrorCode APPLICATION_NOT_SAVED = new ErrorCode(3002);
-        ErrorCode APPLICATION_NOT_UPDATED = new ErrorCode(3003);
-        ErrorCode APPLICATION_NOT_DELETED = new ErrorCode(3004);
-
-    }
-
-    @Section(min = 4000, max = 5000)
-    public interface Service {
-        ErrorCode INVALID_SERVICE = new ErrorCode(4000);
-        ErrorCode SERVICE_NOT_EXIST = new ErrorCode(4001, "Service not found!");
-        ErrorCode SERVICE_NOT_DELETED = new ErrorCode(4002);
-    }
-
     @Section(min = 5000, max = 6000)
     public interface User {
         ErrorCode INVALID_USER = new ErrorCode(5000);
@@ -73,15 +37,10 @@ public final class ErrorCode extends AbstractErrorCode {
         ErrorCode USER_DOES_NOT_HAVE_PERMISSION = new ErrorCode(5005, "User does not have permission for this operation");
         ErrorCode PASSWORD_MISMATCH = new ErrorCode(5006, "Password does not match");
     }
-
-
     @Section(min = 8000, max = 9000)
     public interface Search {
         ErrorCode INVALID_SEARCH_PARAM = new ErrorCode(8000);
         ErrorCode NO_SEARCH_RESULT = new ErrorCode(8001);
 
     }
-
-
-
 }
